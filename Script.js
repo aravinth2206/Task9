7c8d54613d1c8e81765a0ef341306ac8b6502cad
Aravinth <aravinths2206@gmail.com>
2024-01-08 00:17:37 +0530

    First Commit

--- Script.js
+++ Script.js
@@ -3,7 +3,6 @@ let time =document.getElementById('display');
 setTimeout(()=>{
     time.innerHTML=timer--;
     setTimeout(()=>{
-        time.className = "badge rounded-pill bg-light text-dark";
         time.innerHTML=timer--;
         setTimeout(()=>{
             time.innerHTML=timer--;
